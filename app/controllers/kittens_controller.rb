c9f5dec70225353637265f57e811f2b133905277
Crystal <yxcwuu@gmail.com>
2020-03-21 12:20:50 -0500

    Add respond_to JSON method to index and show actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,6 +4,11 @@ class KittensController < ApplicationController
 
     def index
         @kittens = Kitten.all
+
+        respond_to do |format|
+            format.html
+            format.json { render :json => @kittens }
+        end
     end
 
     def new
@@ -22,6 +27,10 @@ class KittensController < ApplicationController
     end
 
     def show
+        respond_to do |format|
+            format.html
+            format.json { render :json => @kitten }
+        end
     end
 
     def edit
