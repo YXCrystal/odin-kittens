e1f8bfbd46a5578c7b429045ff41f5c8fda4590b
Crystal <yxcwuu@gmail.com>
2020-03-21 11:44:31 -0500

    Add name, age, softness, and cuteness presence validations with funny error messages

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,7 @@
 class Kitten < ApplicationRecord
+    validates_presence_of :name, :message => "is blank - Come on, all kittens have names!"
+    validates_presence_of :age, :message => "is blank - How do you not know the age of your kitten?"
+    validates_presence_of :cuteness, :message => "is blank - How dare you not tell us how cute your kitten is?"
+    validates_presence_of :softness, :message => "is blank - We need to know how soft your kitten is (for research purposes)!"
+
 end
