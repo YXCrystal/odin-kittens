7bec886c923d0b937f1835a1b8590edf6cef01c4
Crystal <yxcwuu@gmail.com>
2020-03-21 11:44:03 -0500

    Add RESTful actions to Kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,55 @@
 class KittensController < ApplicationController
+
+    before_action :find_kitten, only: [:show, :edit, :destroy, :update]
+
+    def index
+        @kittens = Kitten.all
+    end
+
+    def new
+        @kitten = Kitten.new
+    end 
+    
+    def create
+        @kitten = Kitten.create(kitten_params)
+        if @kitten.save
+            flash[:notice] = "Kitten has been successfully created!"
+            redirect_to kitten_path(@kitten)
+        else 
+            flash.now[:error] = "Kitten cannot be created"
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @kitten.update(kitten_params)
+        if @kitten.save 
+            flash[:notice] = "Kitten has been successfully update!"
+            redirect_to kitten_path(@kitten)
+        else 
+            flash.now[:error] = "Kitten cannot be updated"
+            render 'new'
+        end
+    end
+
+    def destroy
+        @kitten.destroy 
+        redirect_to kittens_path
+    end
+
+    private 
+
+    def find_kitten
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def kitten_params 
+        params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+    end
 end
